51dd95cb512e00b5645cc953137d2c65d74eff7c
Your Name <you@example.com>
2025-08-11 11:58:43 +0530

    first commit

---
+++ src/app/components/LogoShowcase.tsx
@@ -0,0 +1,43 @@
+export function LogoShowcase() {
+  return (
+    <section className="relative flex z-10 flex-col justify-center mb-0 w-full blur-0 max-md:py-24 max-md:mb-2.5 max-md:max-w-full [background:linear-gradient(275deg,rgba(254,250,238,0.8),rgba(252,214,139,0.8),rgba(249,120,76,0.8),rgba(156,53,109,0.8))] bg-blend-multiply">
+      <div className="flex flex-wrap gap-10 justify-center items-center px-14 py-10 w-full max-md:px-5 max-md:max-w-full">
+        <img
+          src="https://api.builder.io/api/v1/image/assets/TEMP/b8f1c7f6f51c78fe0cd7764348356115ea44d169?placeholderIfAbsent=true&apiKey=1dc25df092204ad2af97e2cc14c5d1e5"
+          alt="Client logo"
+          className="object-contain shrink-0 self-stretch my-auto aspect-[4.26] w-[102px]"
+        />
+        <img
+          src="https://api.builder.io/api/v1/image/assets/TEMP/53a1acd162733e71ca09fe2337498db2fda70a54?placeholderIfAbsent=true&apiKey=1dc25df092204ad2af97e2cc14c5d1e5"
+          alt="Client logo"
+          className="object-contain shrink-0 self-stretch my-auto w-7 aspect-[1.17]"
+        />
+        <img
+          src="https://api.builder.io/api/v1/image/assets/TEMP/606ecd50aecd96600183c56ca3509889796dcd31?placeholderIfAbsent=true&apiKey=1dc25df092204ad2af97e2cc14c5d1e5"
+          alt="Client logo"
+          className="object-contain shrink-0 self-stretch my-auto w-9 aspect-[1.5]"
+        />
+        <img
+          src="https://api.builder.io/api/v1/image/assets/TEMP/0ff00b616c47d4c3922dd3b14b695cb7f539ddd8?placeholderIfAbsent=true&apiKey=1dc25df092204ad2af97e2cc14c5d1e5"
+          alt="Client logo"
+          className="object-contain shrink-0 self-stretch my-auto aspect-[2.62] w-[63px]"
+        />
+        <img
+          src="https://api.builder.io/api/v1/image/assets/TEMP/612238aa28f1d973d08668effe8c43885ba80a51?placeholderIfAbsent=true&apiKey=1dc25df092204ad2af97e2cc14c5d1e5"
+          alt="Client logo"
+          className="object-contain shrink-0 self-stretch my-auto aspect-[3.5] w-[84px]"
+        />
+        <img
+          src="https://api.builder.io/api/v1/image/assets/TEMP/18f1d5e543e41155854305b3c689d1bebe92a25c?placeholderIfAbsent=true&apiKey=1dc25df092204ad2af97e2cc14c5d1e5"
+          alt="Client logo"
+          className="object-contain shrink-0 self-stretch my-auto w-48 aspect-[8]"
+        />
+        <img
+          src="https://api.builder.io/api/v1/image/assets/TEMP/a905cedbc95d5ce509dc725acd3bcf35e73b6530?placeholderIfAbsent=true&apiKey=1dc25df092204ad2af97e2cc14c5d1e5"
+          alt="Client logo"
+          className="object-contain shrink-0 self-stretch my-auto aspect-[3.88] w-[93px]"
+        />
+      </div>
+    </section>
+  );
+}
